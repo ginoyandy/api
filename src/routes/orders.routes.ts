f630b715a1d7654ad17dc64483cd0a7c93721451
Gino Massei <ginomassei@icloud.com>
2022-02-25 09:25:52 -0300

    Think that now, PUT method is working

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,11 +1,9 @@
 import express from 'express';
 import { modifyOrderReport, createOrderObject, getOrderPDF } from '../controllers/orders.controller';
-import { OrderSchema } from '../data/validation-schemas/order.schema';
-import { validateRequest } from '../middlewares/validateRequests';
 
 export const ordersRouter = express.Router();
 
 ordersRouter.get('/pdf/:id', getOrderPDF);
 ordersRouter.post('/excel', createOrderObject);
-ordersRouter.put('/:id', validateRequest(OrderSchema), modifyOrderReport);
+ordersRouter.put('/:id', modifyOrderReport);
 // ordersRouter.get('/pdf');
