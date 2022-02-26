17ec2ada50b1faf4493ae271ab5102ad287274e6
Gino Massei <ginomassei@icloud.com>
2022-02-26 17:14:32 -0300

    gola

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -54,7 +54,7 @@ export const getOrderPDF = async (req: Request, res: Response): Promise<any> => 
 export const getAllOrders = async (req: Request, res: Response): Promise<any> => {
   try {
     return await getOrders()
-      .then((result) => res.status(302).json(result))
+      .then((result) => res.status(200).json(result))
       .catch((error) => res.status(400).json(error));
   } catch (e) {
     return res.status(400).json(e);
